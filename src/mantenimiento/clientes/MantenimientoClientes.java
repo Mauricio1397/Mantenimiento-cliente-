8e2ffd2a80d8a586949f75464b40b4a5d8f16988
Mauricio1397 <83929664+Mauricio1397@users.noreply.github.com>
2021-10-22 00:21:15 -0600

    Carga del proyecto

---
+++ src/mantenimiento/clientes/MantenimientoClientes.java
@@ -0,0 +1,20 @@
+
+package mantenimiento.clientes;
+
+/**
+ *
+ * @author mejia
+ */
+public class MantenimientoClientes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Login login=new Login();
+        login.setVisible(true);
+        
+    }
+    
+}
